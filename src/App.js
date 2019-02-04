6bee72d6d8c23a6c611b05240d6745b5d20c3c77
igreatdev <izudongreat@gmail.com>
2019-02-04 18:42:19 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+import {AddExpense, ExpensesList} from './components/Expenses';
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main role="main" className="container">
+        <div className = "d-flex align-items-center p-3 my-3 text-white-50 bg-primary rounded box-shadow" >
+          <div className="lh-100">
+            <h4 className="text-white">Expenses Saver</h4>
+          </div>
+        </div>
+
+        <Route exact path="/" component={ExpensesList} />
+        <Route path="/add" component={AddExpense} />
+      </main>
     );
   }
 }
